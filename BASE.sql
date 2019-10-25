90f0cd9ae5ec76281132852b619fbbb4915915c7
Your Name <you@example.com>
2019-10-25 01:09:58 -0300

    page cliente

--- BASE.sql
+++ BASE.sql
@@ -15,6 +15,39 @@ CREATE TABLE Personas
 	tipo varchar(50) not null --Puede ser Vendedor, Supervisor, Administrador, Proveedor
 )
 GO
+CREATE TABLE Usuarios
+(
+	   id int not null primary key,
+	   nombre varchar(100) not null,
+	   contrasenia varchar(100) not null
+)
+GO
+CREATE TABLE Vendedores(
+	id int not null primary key identity(1,1),
+	fNacimiento Date not null,
+	fRegistro Datetime not null,
+	sexo char null,
+	idUsuario int null foreign key references Usuarios(id),
+)
+
+GO
+
+CREATE TABLE Clientes(
+	id int not null primary key identity(1,1),
+	fechaNacimiento Date not null,
+	fechaRegistro Datetime not null
+)
+GO
+
+CREATE TABLE Empresa(
+	id int not null primary key identity(1,1),
+	razonSocial varchar(100) not null unique,
+)
+GO
+CREATE TABLE Proveedores(
+	id int not null primary key identity(1,1),
+)
+GO
 CREATE TABLE Telefonos
 (
 	id int not null foreign key references Personas(id),
@@ -26,7 +59,7 @@ GO
 CREATE TABLE Emails
 (
 	id int not null primary key foreign key references Personas(id),
-	email varchar(100) null unique
+	email varchar(100) null unique 
 )
 
 /*
@@ -49,6 +82,25 @@ CREATE TABLE Ciudades
 	nombre varchar(50) not null
 )
 */
+GO       
+CREATE TABLE Marcas(
+	id int not null primary key identity(1,1),
+	nombre varchar(100) not null unique
+)
+GO       
+CREATE TABLE Productos(
+	codigo varchar(100) not null primary key,
+	nombre varchar(100) not null,
+	imagen varchar(200) not null,
+	idMarca int not null foreign key references Marcas(id)
+)
+GO
+CREATE TABLE Stock(
+	codigoProduct varchar(100) not null foreign key references Productos(codigo),
+	minimo int not null,
+	maximo int null,
+	primary key(codigoProduct)	
+)
 GO
 CREATE TABLE Direcciones(
 	id int not null primary key foreign key references Personas(id),
@@ -57,6 +109,70 @@ CREATE TABLE Direcciones(
 	departamento int null,
 	ciudad varchar(100) null,
 	provincia varchar(100) null,
-	pais varchar(100) null,
+	pais varchar(100) null
 	--idCiudad int null foreign key references Ciudades(id)
 )
+
+GO
+CREATE TABLE DetalleVentas(
+		id int not null,     --PUEDE EXISTIR MAS DE UNA VENTA
+		idCodigoFactura int not null,
+		idProducto varchar(100) not null foreign key references Productos(codigo),
+		codigoFactura int not null,
+		precioU money not null,
+		cantidad int not null, --CREAR CHECK MAYOR CERO
+		total money not null,
+		subtotal money not null,
+		iva float null
+		)
+GO
+CREATE TABLE Tipo_Factura(
+	id int not null primary key identity(1,1),
+	tipo varchar(50) not null
+)
+GO
+CREATE TABLE  MedioPago(
+	id int not null primary key identity (1,1),
+	medio varchar(100) not null
+)
+GO        
+CREATE TABLE DetalleCompras(
+	id int not null,
+	codigoProducto varchar(100) not null foreign key references Productos(codigo), 
+	codigoFactura int not null,
+	precioU money not null,
+	cantidad int not null, --CREAR CHECK MAYOR CERO
+	total money not null,
+	subtotal money not null,
+	iva float null
+)
+GO
+CREATE TABLE Compras(
+	codigo int not null primary key identity(1,1),
+	idTipoFactura int not null foreign key references Tipo_Factura(id),
+	idMedioPago int not null foreign key references MedioPago(id),
+	idEmpresa int not null foreign key references Empresa(id),
+	idProveedor int not null foreign key references Proveedores(id),
+	fecha datetime not null,
+	total money not null,
+	subtotal money not null,
+	nota varchar(100) null,
+	idDetalleCompra int not null
+)
+
+GO
+CREATE TABLE Facturacion(
+	codigo int not null primary key identity(1,1),
+	idTipoFactura int not null foreign key references Tipo_Factura(id),
+	idMedioPago int not null foreign key references MedioPago(id),
+	idCliente int not null foreign key references Clientes(id),
+	idVendedor int not null foreign key references Vendedores(id),
+	fecha DateTime not null,
+	total money not null,
+	subtotal money not null,
+	nota varchar(100) null,
+	idVenta int not null,
+	CHECK(subtotal>0),
+	CHECK(total>0)
+)
+
--- CommercialSystem/PageCliente.aspx
+++ CommercialSystem/PageCliente.aspx
@@ -1,6 +1,103 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="PageCliente.aspx.cs" Inherits="CommercialSystem.PageCliente" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
 
-    <img  src=""/>
-  
+    <style>
+       
+        .menuABM{
+            float:left;
+            margin: 0px 0px 15px 15px;
+        }
+        .buscarABM{
+            text-align:center;
+            margin-left:60%;
+            margin-right:10px;
+            text-anchor:start;
+            margin-top:10px;   
+        }
+       * { 
+	margin: 0; 
+	padding: 0; 
+        }
+    body { 
+	    font: 14px/1.4 Georgia, Serif; 
+    }
+    #page-wrap {
+	    margin: 50px;
+    }
+    p {
+	    margin: 20px 0; 
+    }   
+
+	/* 
+	Generic Styling, for Desktops/Laptops 
+	*/
+	table { 
+		width: 100%; 
+		border-collapse: collapse; 
+	}
+	/* Zebra striping */
+	tr:nth-of-type(odd) { 
+		background: #eee; 
+	}
+	th { 
+		background: #333; 
+		color: white; 
+		font-weight: bold; 
+	}
+	td, th { 
+		padding: 6px; 
+		border: 1px solid #ccc; 
+		text-align: left; 
+	}
+
+       </style>
+
+
+
+    <div class="container body-content">
+        <a href="#" class="menuABM" title="Nuevo"><img src="Imagenes/new-file_40454.png"><div>Nuevo</div></a> 
+        <a href="#" class="menuABM" title="Editar"><img src="Imagenes/Edit.png"><div>Editar</div></a>
+        <a href="#" class="menuABM" title="Eliminar"><img src="Imagenes/Delete.png"><div>Eliminar</div></a>
+        <a href="#" class="menuABM" title="Cancelar"><img src="Imagenes/Cancel.png"><div>Cancelar</div></a> 
+    </div>
+    <div></div>
+    <asp:TextBox runat="server" AutoPostBack="true" BorderStyle="Solid" BorderColor="#0066ff" Height="30px" Width="457px" CssClass="buscarABM"></asp:TextBox><asp:Button runat="server" BorderStyle="Groove" Width="112px" BorderWidth="2px" Font-Bold="True" Font-Italic="False" Font-Size="Medium" ForeColor="Black" Text="Buscar"/>
+
+
+
+	<h1>Table</h1>		
+	<table>
+		<tr>
+			<th>COLUMNA 1</th>
+			<th>COLUMNA 2</th>
+			<th>COLUMNA 3</th>
+		</tr>
+		<tr>
+			<td>FILA 1</td>
+			<td>FILA 2</td>
+			<td>FILA 3</td>
+		</tr>
+		<tr>
+		    <td>FILA 1</td>
+			<td>FILA 2</td>
+			<td>FILA 3</td>
+		</tr>
+		<tr>
+		    <td>FILA 1</td>
+			<td>FILA 2</td>
+			<td>FILA 3</td>
+		</tr>
+		<tr>
+		    <td>FILA 1</td>
+			<td>FILA 2</td>
+			<td>FILA 3</td>
+		</tr>
+		<tr>
+		    <td>FILA 1</td>
+			<td>FILA 2</td>
+			<td>FILA 3</td>
+		</tr>
+	</table>	
+
+
 </asp:Content>
